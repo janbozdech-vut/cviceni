85fbef4c6a3365b71e54c14656d14ed364bbc69c
unknown <Student@PC-SE5115-05.utko.feec.vutbr.cz>
2025-10-02 12:49:10 +0200

    CV03-complete

---
+++ CV03/Bonus kmplxcsl/Bonus kmplxcsl/Bonus kmplxcsl.cpp
@@ -0,0 +1,33 @@
+﻿// Bonus kmplxcsl.cpp : Defines the entry point for the application.
+//
+
+#include "Bonus kmplxcsl.h"
+
+using namespace std;
+
+int main()
+{
+	int Ar;
+	int Ai;
+	int Br;
+	int Bi;
+
+	printf("Zadej hodnotu komplexniho cisla A = ");
+	scanf("%d", &Ar);
+	printf(" + ");
+	scanf("%d", &Ai);
+	printf("i.\n");
+
+	printf("Zadej hodnotu komplexniho cisla B = ");
+	scanf("%d", &Br);
+	printf(" + ");
+	scanf("%d", &Bi);
+	printf("i.\n");
+
+	int Sr = Ar + Br;
+	int Si = Ai + Bi;
+
+	printf("Soucet techto cisel je roven: %d + %di", Sr, Si);
+	
+	return 0;
+}
---
+++ CV03/Bourka/Bourka/Bourka.cpp
@@ -0,0 +1,22 @@
+﻿// Bourka.cpp : Defines the entry point for the application.
+//
+
+#include "Bourka.h"
+
+using namespace std;
+
+int main()
+{
+	int rychlost = 340;
+	int cas;
+	int vzdalenost;
+
+	printf("Po jake dobe po blesku se ozval hrom? (s) ");
+	scanf("%d", &cas);
+
+	vzdalenost = rychlost * cas;
+
+	printf("Bourka je ve vydalenosti %d m.\n", vzdalenost);
+
+	return 0;
+}
