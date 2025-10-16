40c31f09f884c5735844a06e6382f3d7a08f2ac6
unknown <Student@PC-SE5115-05.utko.feec.vutbr.cz>
2025-10-16 12:28:08 +0200

    zdar

---
+++ CV05/vektory/vektory/vektory.cpp
@@ -0,0 +1,62 @@
+﻿// vektory.cpp : Defines the entry point for the application.
+//
+
+#include "vektory.h"
+#include "math.h"
+
+using namespace std;
+
+
+
+void soucet(double a[3], double b[3], double c[3]){
+	c[0] = a[0] + b[0];
+	c[1] = a[1] + b[1];
+	c[2] = a[2] + b[2];
+}
+
+double velikost(double a[3]) {
+	return sqrt(pow(a[0],2) + pow(a[1],2) + pow(a[2],2));
+}
+
+double skalarniSoucin(double a[3], double b[3]) {
+	double soucin = 0;
+
+		for (int i = 0; i < 3; i++) {
+			soucin = soucin + a[i] * b[i];
+		};
+	return soucin;
+}
+
+void vektorovySoucin(double a[3], double b[3], double d[3]) {
+	d[0] = (a[1] * b[2]) - (a[2] * b[1]);
+	d[1] = (a[2] * b[0]) - (a[0] * b[2]);
+	d[2] = (a[0] * b[1]) - (a[1] * b[0]);
+}
+
+
+int main()
+{
+	double a[3], b[3], c[3], d[3];
+
+	printf("\nVektor A (3 hodnoty oddelene carkou): ");
+	scanf_s("%lf, %lf, %lf", &a[0], &a[1], &a[2]);
+
+	printf("\nVektor B (3 hodnoty oddelene carkou): ");
+	scanf_s("%lf, %lf, %lf", &b[0], &b[1], &b[2]);
+
+	printf("\n");
+	soucet(a, b, c);
+	printf("Soucet A + B: (%lf, %lf, %lf)\n", c[0], c[1], c[2]);
+
+	printf("\n");
+	printf("Velikost A: %lf\n", velikost(a));
+
+	printf("\n");
+	printf("Sklarni soucin A*B: %lf\n", skalarniSoucin(a, b));
+
+	printf("\n");
+	vektorovySoucin(a, b, d);
+	printf("Vektorovy soucin A x B: (%lf, %lf, %lf)\n", d[0], d[1], d[2]);
+
+	return 0;
+}
